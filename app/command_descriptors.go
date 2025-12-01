f96a2b116df6dc1b0163db5e103ebbbb432726d6
Siddharth-Nema <siddharthnema2308@gmail.com>
2025-12-01 22:24:52 +0530

    Command descriptors

---
+++ app/command_descriptors.go
@@ -0,0 +1,8 @@
+package main
+
+// CommandDescriptions maps command names to their descriptions
+var CommandDescriptions = map[string]string{
+	"echo": "echo is a shell builtin",
+	"exit": "exit is a shell builtin",
+	"type": "type is a shell builtin",
+}
--- app/main.go
+++ app/main.go
@@ -29,10 +29,15 @@ func main() {
 				fmt.Printf("%s ", tokens[i])
 			}
 			fmt.Println()
+		} else if command == "type" {
+			if desc, exists := CommandDescriptions[tokens[1]]; exists {
+				fmt.Fprintf(os.Stdout, "%s\n", desc)
+			} else {
+				fmt.Fprintf(os.Stdout, "%s: not found\n", tokens[1])
+			}
 		} else {
 			fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
 		}
-
 	}
 
 }
